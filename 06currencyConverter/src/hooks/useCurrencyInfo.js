56f872e2e30c28360b3b21972cbad9ce636d9b7c
vinayprtap-17 <mailto.vinaypratap@gmail.com>
2024-06-03 18:05:02 +0530

    project 5,6&7

---
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,13 @@
+import { useCallback, useRef, useState,useEffect } from "react";
+
+function useCurrencyInfo(currency){
+    const [data,setdata]=useState({})
+    useEffect(()=>{
+        fetch=(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/usd.json`)
+        .then((res)=>res.json()).then((res)=>setdata(res=>[currency]))
+        
+    },[currency])
+    console.log(data);
+    return data;
+}
+export default useCurrencyInfo;
---
+++ bgchanger/src/components/colortray.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function colortray() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default colortray
